de2269d8fc8e9d4d292e1e78346881a639b869ec
Vadim Ruban <vruban@rocketfuel.com>
2018-02-21 14:59:44 +0200

    fix(no-describe-variables): Format code accoring to standartjs

--- test/rules/no-describe-variables.js
+++ test/rules/no-describe-variables.js
@@ -4,8 +4,8 @@ var rule = require('../../lib/rules/no-describe-variables')
 var RuleTester = require('eslint').RuleTester
 
 var eslintTester = new RuleTester({
-  "parserOptions": {
-    "ecmaVersion": 6
+  'parserOptions': {
+    'ecmaVersion': 8
   }
 })
 
@@ -16,7 +16,7 @@ eslintTester.run('no-describe-variables', rule, {
     'fdescribe("My suite", function() { it("works", function() {}); });',
     'describe("My suite", () => { it("works", function() {}); });',
     'xdescribe("My suite", () => { it("works", function() {}); });',
-    'fdescribe("My suite", () => { it("works", function() {}); });',
+    'fdescribe("My suite", () => { it("works", function() {}); });'
   ],
   invalid: [
     {
